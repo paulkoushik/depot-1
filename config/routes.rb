329d40a8c0e8b5623b23b91c2c48e45c2076bae3
Götz Pfeiffer <goetz.pfeiffer@nuigalway.ie>
2020-11-09 13:27:31 +0000

    added cart scaffold and concern

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :carts
   root to: 'store#index', as: 'store'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_174615) do
+ActiveRecord::Schema.define(version: 2020_11_09_132728) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
